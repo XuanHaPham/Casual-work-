29bf9669b1944ed182338d3725063f18e97c03ba
VuHN <vu23071997@gmail.com>
2019-04-16 20:55:33 +0700

    VuHN_UP Code

---
+++ CasualWord/CasualWord/CasualWord/Helper/NotificationObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CasualWord.Helper
+{
+    public class NotificationObject : INotifyPropertyChanged
+    {
+        //public event PropertyChangedEventHandler PropertyChanged;
+        //protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
+        //protected virtual void SetProperty<T>(ref T member, T val,
+        //    [CallerMemberName] string propertyName = null)
+        //{
+        //    if (object.Equals(member, val)) return;
+
+        //    member = val;
+        //    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        //}
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected bool SetProperty<T>(ref T field, T value, string propertyName)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+
+    }
+}
---
+++ CasualWord/CasualWord/CasualWord/Helper/SimpleCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace CasualWord.Helper
+{
+    public class SimpleCommand : ICommand
+    {
+        Action _execute;
+        public SimpleCommand(Action execute)
+        {
+            this._execute = execute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+    }
+}
