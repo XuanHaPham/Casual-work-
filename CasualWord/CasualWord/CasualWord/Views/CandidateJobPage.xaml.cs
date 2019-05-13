7b820531df24f88f379a27a504f6261b89e0cb3e
VuHN <vu23071997@gmail.com>
2019-05-13 14:27:53 +0700

    VuHN_Up code v.2

--- CasualWord/CasualWord/CasualWord/Views/CandidateJobPage.xaml.cs
+++ CasualWord/CasualWord/CasualWord/Views/CandidateJobPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,19 @@ namespace CasualWord.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CandidateJobPage : ContentPage
 	{
-        public List<int> testList;
+        public ObservableCollection<int> testList;
 		public CandidateJobPage ()
 		{
 			InitializeComponent ();
-            testList = new List<int>();
+            testList = new ObservableCollection<int>();
+            testList.Add(1);
+            testList.Add(1);
+            testList.Add(1);
             testList.Add(1);
             testList.Add(1);
             listView.ItemsSource = testList;
+            MyListView.ItemsSource = testList;
+            MyListHistory.ItemsSource = testList;
 
         }
 	}
